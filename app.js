b569522a481c94394d4ac74bfe7849cba393ef75
Ilya Antipenko <ilya@antipenko.pp.ua>
2019-07-26 10:05:24 +0300

    Add "HVAC mode is" condition

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ class GreeHVAC extends Homey.App {
 
     onInit() {
         this.log('Gree HVAC app is up and running...');
+
+        // Register conditions for flows
+        this._conditionHVACModeIs = new Homey.FlowCardCondition('hvac_mode_is')
+            .register()
+            .registerRunListener((args, state) => {
+                return args.mode === args.device.getCapabilityValue('hvac_mode');
+            });
     }
 }
 
