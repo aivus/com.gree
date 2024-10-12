a9ff8cb68b4c7fd7c74f5e1be660516e2294a2ff
Illia Antypenko <illia@antypenko.dev>
2024-10-12 22:17:55 +0200

    Update gree-hvac-client to allow autodetece of encryption mode (#171)

--- drivers/gree_cooper_hunter_hvac/device.js
+++ drivers/gree_cooper_hunter_hvac/device.js
@@ -82,7 +82,6 @@ class GreeHVACDevice extends Homey.Device {
 
         const deviceData = this.getData();
         const settings = this.getSettings();
-        const encryptionVersion = this.mapEncryptionModeSetting(settings.encryption_mode);
 
         this.log('[find devices]', 'Finding device with mac:', deviceData.mac);
 
@@ -104,7 +103,6 @@ class GreeHVACDevice extends Homey.Device {
                 pollingInterval: POLLING_INTERVAL,
                 pollingTimeout: POLLING_TIMEOUT,
                 connectTimeout: CONNECT_TIMEOUT,
-                encryptionVersion,
             });
 
             this._registerClientListeners();
@@ -676,26 +674,7 @@ class GreeHVACDevice extends Homey.Device {
             }
         }
 
-        if (changedKeys.indexOf('encryption_mode') > -1) {
-            this.log('Changing the encryption mode setting from', oldSettings.encryption_mode, 'to', newSettings.encryption_mode);
-            this.homey.setTimeout(this.reconnect.bind(this), 1000);
-        }
-
         return Promise.resolve();
-    }
-
-    mapEncryptionModeSetting(encryptionMode) {
-        switch (encryptionMode) {
-            // not implemented yet
-            case 'auto':
-            case 'v1':
-            default:
-                // AES-ECB
-                return 1;
-            case 'v2':
-                // AES-GCM
-                return 2;
-        }
     }
 
     reconnect() {
